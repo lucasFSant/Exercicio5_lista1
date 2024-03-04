2d501cfcd102c0e08a2e73f14323c23bec951b54
ra2357028 <ra2357028@LDI305>
2024-03-04 07:31:24 -0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿double[] vet1 = new double[5];
+double[] vet2 = new double[5];
+string? n;
+
+for (int i = 0; i < vet1.Length; i++)
+{
+    Console.Write((i+1) + "º valor do vet1: ");
+    n = Console.ReadLine();
+    Double.TryParse(n, out vet1[i]);
+
+}
+
+for (int i = 0; i < vet2.Length; i++)
+{
+    Console.Write((i + 1) + "º valor do vet2: ");
+    n = Console.ReadLine();
+    Double.TryParse(n, out vet2[i]);
+
+}
+
+for (int i = 0; i < vet1.Length; i++)
+{
+    Console.WriteLine("Soma dos " + (i +1)+ "º valores: " + (vet1[i] + vet2[i]));
+    Console.WriteLine("Diferença dos " + (i+1) + "º valores: " + (vet1[i] - vet2[i]));
+    Console.WriteLine("Produto dos " + (i+1) + "º valores: " + (vet1[i] * vet2[i]));
+    Console.WriteLine("Divisão dos " + (i+1) + "º valores: " + (vet1[i] / vet2[i]));
+    Console.WriteLine();
+}
